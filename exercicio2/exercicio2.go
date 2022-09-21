0c379a0b011e377ba990a112f6ee233b414c4f25
Natália Salvino André <55745942+natalia-sa@users.noreply.github.com>
2022-09-21 16:06:29 -0300

    Update exercicio2.go

--- exercicio2/exercicio2.go
+++ exercicio2/exercicio2.go
@@ -1,7 +1,7 @@
 // implemente uma go routine que gere valores aleatorios
 // enquanto uma segunda verifica se os valores sao pares ou impares
 package main
-// go é uma chamada a uma função, n pode ta atribuido a variavel
+
 import (
 	"fmt"
 	"math/rand"
@@ -9,8 +9,6 @@ import (
 
 // o escalonador escolhe aleatoriamente qual fluxo executar
 // esse codigo da deadlock
-// no momento que a thread main trava ela executa mais alguma coisa? acho que n.
-// eka trava e so desbloqueia se tiver alguem fazendo um produce
 func main() {
 	number := make(chan int)
 	x := generateRandomValue()
